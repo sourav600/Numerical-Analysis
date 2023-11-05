f024edfb84563a39ec8dbaa114db9be32f9b9e58
Moinul Islam Sourav <moinulmd306@gmail.com>
2023-11-05 21:56:09 +0600

    Bisection method

---
+++ bisection.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double func(double x){
+    return (x*x*x)-(3*x)-5;
+}
+
+
+void bisection(double a, double b){
+    if(func(a) * func(b) >= 0){
+        cout<<"Invalid value of a, b\n";
+        return;
+    }
+
+    double x;
+    while(b-a >= 0.01){
+        x = (a+b)/2.00;
+        if(func(x)==0.00) break;
+        else if(func(x) * func(a) <= 0) b=x;
+        else a=x;
+    }
+    cout<<"The root is: "<<x<< endl;
+}
+int main(){
+    double a=2.0, b=3.0;
+    bisection(a,b);
+
+return 0;
+}
+
+
+
